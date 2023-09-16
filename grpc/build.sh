107148bca394ef4f316749d107a824ae48572585
syndafloden <vladkoval805@gmail.com>
2023-09-17 06:50:05 +0700

    vot sha uje pochti

--- grpc/build.sh
+++ grpc/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 protoc --go-grpc_out=../s3_service --go_out=../s3_service grpc.proto
-python3 -m grpc_tools.protoc -I. --python_out=../gateway --grpc_python_out=../gateway grpc.proto
 protoc --java_out=../user_service grpc.proto
