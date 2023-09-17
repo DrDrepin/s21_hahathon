53c2ba8bef43dced3ea4beec1296ab4281051880
Galion Merling <galionme@in-c3.nsk.21-school.ru>
2023-09-17 08:45:07 +0700

    boje_spasi

--- gateway/main.py
+++ gateway/main.py
@@ -35,42 +35,40 @@ def get_bad_answer():
 
 ### use class
 
-# class BodyRead(BaseModel): 
-#     path: str
-#     skip: int
-#     tack: int
-
 class File(BaseModel):
     path: str  
-    data: str
+    data: bytes
 
 class User(BaseModel):
     login: str
     password: str
-    workspace_id: str
 
 class ReadWorkSpace(BaseModel):
     path: str  
     limit: int
     offset: int 
-
-# class DataBase(BaseModel):
-#     folder: str  
-#     path: str
-#     data: str 
+ 
+class Person(BaseModel):
+    login: str
+    password: str
 
 class DataFile(BaseModel):
     data: str
+
+Users = [
+    Person(login= 'usr', password=get_password_hash('123')),
+    Person(login= 'adm', password=get_password_hash('123'))
+]
 
 ### testing 
 
 ### helpers
 
-# def authenticate_user(login: str, password: str):
-#     for user in users:
-#         if user.login == login and pwd_context.verify(password, user.password):
-#             return user
-#     return None
+def authenticate_user(login: str, password: str):
+    for user in Users:
+        if user.login == login and pwd_context.verify(password, user.password):
+            return user
+    return None
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
@@ -88,52 +86,83 @@ def set_answer_good(response):
 ### use workspace and user
 
 # check user and set new token - no auth
-# @app.post('/token')
-# def login_for_access_token(task: User):
-#     login = task.login
-#     password = task.password
-#     user = authenticate_user(login, password)
-#     if not user:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail='Неправильный логин или пароль',
-#             headers={'Authorization': 'Bearer'},
-#         )
-#     access_token = create_access_token(
-#         data={'sub': user.login}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     )
-#     return {'access_token': access_token, 'token_type': 'Bearer'}
+@app.post('/token')
+def login_for_access_token(task: Person):
+    login = task.login
+    password = task.password
+    user = authenticate_user(login, password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Неправильный логин или пароль',
+            headers={'Authorization': 'Bearer'},
+        )
+    access_token = create_access_token(
+        data={'sub': user.login}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+    return {'access_token': access_token, 'token_type': 'Bearer'}
 
 # create workspace and create user - no auth 
 
-@app.post('/create_user')
-def create_user(task: User):
-    status = gRPC_CreateUser(task.login, task.password, task.workspace_id)
+@app.post('/create_user/{workspace_id}')
+def create_user(task: User, workspace_id: str):
+    logging.info('1')
+    status = gRPC_CreateUser(task.login, task.password, workspace_id)
     if status == True:
         access_token = create_access_token(
             data={'sub': task.login}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         )
         return {'access_token': access_token, 'token_type': 'Bearer'}
-    return {'access_token': 'false', 'token_type': 'Bearer'}
+    return {'access_token': False, 'token_type': 'Bearer'}
 
-# @app.post('/create_workspace')
-# def create_workspace(workspace: WorkSpace):
-#     all_workspace.append(WorkSpace(login=workspace.login, password=get_password_hash(workspace.password), workspace_name=workspace.workspace_name))
+@app.get('/create_workspace/{workspace_name}')
+def create_workspace(workspace_name: str):
+    logging.info('1')
+    status = gRPC_CreateWorkspace(workspace_name)
+    if status == True:
+        return {'data': True, 'status': 'OK', 'code': 200}
+    return {'data': False, 'status': 'ERROR', 'code': 501}
 
-#     access_token = create_access_token(
-#         data={'sub': workspace.login}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     )
-#     return {'access_token': access_token, 'token_type': 'bearer'}
+@app.post('/upload_file/{workspace_id}')
+def upload_file(task: File, workspace_id: str):
+    logging.info('1')
+    status = gRPC_CreateFile(task.path, workspace_id, task.data)
+    if status == True:
+        return {'data': True, 'status': 'OK', 'code': 200}
+    return {'data': False, 'status': 'ERROR', 'code': 501}
 
 # @app.post('/read_path')
 # def read_path(task: ReadWorkSpace, current_user: User = Depends(oauth2_scheme)):
-#     path = task.path
-#     offset = task.offset
-#     limit = task.limit - 1
-#     data = 
-#     return {'data': data, 'status': 'OK', 'code': 200}
+#     status = ()
+#     if status == True:
+#         return {'data': True, 'status': 'OK', 'code': 200}
+#     return {'data': False, 'status': 'OK', 'code': 200}
+
+
+@app.get('/give_file/{path}')
+def give_file(path: str, current_user: User = Depends(oauth2_scheme)):
+    data = gRPC_GetFile(path)
+    return {'data': data, 'status': 'OK', 'code': 200}
+
+@app.get('/create_folder/{folder}')
+def create_folder(folder: str, current_user: User = Depends(oauth2_scheme)):
+    return {'data': folder, 'status': 'OK', 'code': 200}
+
+@app.get('/give_folder/{folder}')
+def give_folder(folder: str, current_user: User = Depends(oauth2_scheme)):
+    return {'data': folder, 'status': 'OK', 'code': 200}
+
+
+### use folders
+
+
+# @app.get('/delete_folder/{folder}')
+# def delete_folder(folder: str, current_user: User = Depends(oauth2_scheme)):
+#     return {'data': folder, 'status': 'OK', 'code': 200}
+
 
 ### use files
+
 
 # @app.get('/delete_file/{path}')
 # def upload_file(path: str, current_user: User = Depends(oauth2_scheme)):
@@ -143,10 +172,7 @@ def create_user(task: User):
 #             return {'data': path, 'status': 'OK', 'code': 200}
 #     return get_bad_answer()
 
-# @app.post('/upload_file/{path}')
-# def upload_file(task: DataFile, path: str, current_user: User = Depends(oauth2_scheme)):
-#     # data = 
-#     return {'data': data, 'status': 'OK', 'code': 200}
+
 
 # @app.post('/replace_file/{path}')
 # def replace_file(task: DataFile, path: str, current_user: User = Depends(oauth2_scheme)):
@@ -157,24 +183,6 @@ def create_user(task: User):
 #             return {'data': path, 'status': 'OK', 'code': 200}
 #     return get_bad_answer()
 
-@app.get('/give_file/{path}')
-def give_file(path: str, current_user: User = Depends(oauth2_scheme)):
-    data = gRPC_GetFile(path)
-    return {'data': data, 'status': 'OK', 'code': 200}
-
-### use folders
-
-@app.get('/create_folder/{folder}')
-def create_folder(folder: str, current_user: User = Depends(oauth2_scheme)):
-    return {'data': folder, 'status': 'OK', 'code': 200}
-
-# @app.get('/delete_folder/{folder}')
-# def delete_folder(folder: str, current_user: User = Depends(oauth2_scheme)):
-#     return {'data': folder, 'status': 'OK', 'code': 200}
-
-@app.get('/give_folder/{folder}')
-def give_folder(folder: str, current_user: User = Depends(oauth2_scheme)):
-    return {'data': folder, 'status': 'OK', 'code': 200}
 
 ### use grpc
 
@@ -187,18 +195,18 @@ def gRPC_CreateUser(login: str, password: str, workspace_id: str):
         logging.info(responce)
     return responce
 
-def gRPC_CreateWorkspace():
+def gRPC_CreateWorkspace(name: str):
     with grpc.insecure_channel('localhost:8785') as channel:
         stub = grpc_pb2.TransmissionStub(channel)
-        req = pb2.Path(path="go")
+        req = pb2.Workspace(name=name)
         responce = stub.CreateWorkspace(req)
         logging.info(responce)
     pass
 
-def gRPC_CreateFile():
+def gRPC_CreateFile(path, workspace_id, buffer):
     with grpc.insecure_channel('localhost:8785') as channel:
         stub = grpc_pb2.TransmissionStub(channel)
-        req = pb2.Path(path="go")
+        req = pb2.File(path=path, workspace_id=workspace_id, buffer=buffer)
         responce = stub.CreateFile(req)
         logging.info(responce)
     pass
