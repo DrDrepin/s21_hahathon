8678babaca9f27f8772a602c3043663a989a68dd
Galion Merling <galionme@in-c3.nsk.21-school.ru>
2023-09-17 11:33:09 +0700

    up_jwt

--- gateway/main.py
+++ gateway/main.py
@@ -26,14 +26,19 @@ app = FastAPI(
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
+#
+# JWT format
+# X.X.X 
+# {
+#   "login": "usr",
+#   "workspace_name": "name",
+#   "password": "123",
+#   "exp": 1694926432
+# }
+#
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/token') 
 
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
-
-def get_bad_answer():
-    return {'data': False, 'status': 'ERROR', 'code': 501}
-
+# module OBJECT #
 class File(BaseModel):
     data: bytes
 
@@ -49,7 +54,15 @@ class Person(BaseModel):
 class TokenData(BaseModel):
     login: str 
     workspace_name: str 
+    password: str
     exp: int
+
+# module HELP #
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def get_bad_answer():
+    return {'data': False, 'status': 'ERROR', 'code': 501}
 
 def create_access_token(data: dict, workspace: str, password: str, expires_delta: timedelta = None):
     to_encode = data.copy()
@@ -73,9 +86,7 @@ def decode_jwt(token):
     except jwt.ExpiredSignatureError:
         return None
 
-
-### use workspace and user
-
+# module API #
 @app.post('/token')
 def login_for_access_token(task: Person):
     login = task.login
@@ -93,158 +104,60 @@ def login_for_access_token(task: Person):
     )
     return {'access_token': access_token, 'token_type': 'Bearer'}
 
-# @app.post('/create_user/{workspace_id}')
-# def create_user(task: User, workspace_id: str):
-#     status = gRPC_CreateUser(task.login, task.password, workspace_id)
-#     if status == True:
-#         access_token = create_access_token(
-#             data={'sub': task.login}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#         )
-#         return {'access_token': access_token, 'token_type': 'Bearer'}
-#     return {'access_token': False, 'token_type': 'Bearer'}
-
-# @app.get('/create_workspace')
-# def create_workspace(current_user: TokenData = Depends(oauth2_scheme)):
-#     token = decode_jwt(current_user)
-#     workspace_name = token.workspace_name
-#     status = gRPC_CreateWorkspace(workspace_name)
-#     if status == True:
-#         return {'data': True, 'status': 'OK', 'code': 200}
-#     return get_bad_answer()
-
-# @app.post('/upload_file/{path}')
-# def upload_file(task: File, path: str, current_user: TokenData = Depends(oauth2_scheme)):
-#     token = decode_jwt(current_user)
-#     User = gRPC_ReadUser(token.login, token.password, token.workspace_name)
-#     if not User: 
-#         return get_bad_answer()
-#     File = gRPC_GetFile(workspace_id, path)
-#     if not File:
-#         return get_bad_answer()
-#     return {'data': File, 'status': 'OK', 'code': 200}
-
-# @app.get('/give_file/{path}')
-# def give_file(path: str, current_user: TokenData = Depends(oauth2_scheme)):
-#     token = decode_jwt(current_user)
-#     User = gRPC_ReadUser(token.login, token.password, token.workspace_name)
-#     if not User: 
-#         return get_bad_answer()
-#     File = gRPC_GetFile(User.workspace_id, path)
-#     if not File:
-#         return get_bad_answer()
-#     return {'data': File, 'status': 'OK', 'code': 200}
-
-# @app.get('/give_folder/{path}')
-# def give_folder(path=str, current_user: TokenData = Depends(oauth2_scheme)):
-#     token = decode_jwt(current_user)
-#     User = gRPC_ReadUser(token.login, token.password, token.workspace_name)
-#     if not User: 
-#         return get_bad_answer() 
-#     File = gRPC_GetFolder(User.workspace_id, path)
-#     if not File:
-#         return get_bad_answer()
-#     return {'data': File, 'status': 'OK', 'code': 200}
-
-
-#######
-
 @app.post('/create_user/{workspace_id}')
-def create_user(workspace_id: str):
-    login = 'adm'
-    password = '123'
-    status = gRPC_CreateUser(login, password, workspace_id)
+def create_user(task: User, workspace_id: str):
+    status = gRPC_CreateUser(task.login, task.password, workspace_id)
     if status == True:
         access_token = create_access_token(
-            data={'sub': login}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+            data={'sub': task.login}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         )
         return {'access_token': access_token, 'token_type': 'Bearer'}
     return {'access_token': False, 'token_type': 'Bearer'}
 
-@app.get('/create_workspace/{workspace_name}')
-def create_workspace(workspace_name: str):
-    login = 'adm'
-    password = '123'
+@app.get('/create_workspace')
+def create_workspace(current_user: TokenData = Depends(oauth2_scheme)):
+    token = decode_jwt(current_user)
+    workspace_name = token.get('workspace_name')
     status = gRPC_CreateWorkspace(workspace_name)
     if status == True:
         return {'data': True, 'status': 'OK', 'code': 200}
     return get_bad_answer()
 
 @app.post('/upload_file/{path}')
-def upload_file(file: File, path: str, workspace_id: str):
-    login = 'adm'
-    password = '123'
-    File = gRPC_GetFile(workspace_id, path)
+def upload_file(task: File, path: str, current_user: TokenData = Depends(oauth2_scheme)):
+    token = decode_jwt(current_user)
+    User = gRPC_ReadUser(token.get('login'), token.get('password'), token.get('workspace_name'))
+    if not User: 
+        return get_bad_answer()
+    File = gRPC_CreateFile(path, User.workspace_id, File.data)
     if not File:
         return get_bad_answer()
     return {'data': File, 'status': 'OK', 'code': 200}
 
 @app.get('/give_file/{path}')
-def give_file(path: str, workspace_id: str):
-    login = 'adm'
-    password = '123'
-    File = gRPC_GetFile(workspace_id, path)
+def give_file(path: str, current_user: TokenData = Depends(oauth2_scheme)):
+    token = decode_jwt(current_user)
+    User = gRPC_ReadUser(token.get('login'), token.get('password'), token.get('workspace_name'))
+    if not User: 
+        return get_bad_answer()
+    File = gRPC_GetFile(User.workspace_id, path)
     if not File:
         return get_bad_answer()
     return {'data': File, 'status': 'OK', 'code': 200}
 
 @app.get('/give_folder/{path}')
-def give_folder(workspace_id: str, path=str):
-    login = 'adm'
-    password = '123'
-    File = gRPC_GetFolder(workspace_id, path)
+def give_folder(path=str, current_user: TokenData = Depends(oauth2_scheme)):
+    token = decode_jwt(current_user)
+    User = gRPC_ReadUser(token.get('login'), token.get('password'), token.get('workspace_name'))
+    if not User: 
+        return get_bad_answer() 
+    File = gRPC_GetFolder(User.workspace_id, path)
     if not File:
         return get_bad_answer()
     return {'data': File, 'status': 'OK', 'code': 200}
 
 
-
-
-
-
-
-
-
-# @app.post('/read_path')
-# def read_path(task: ReadWorkSpace, current_user: User = Depends(oauth2_scheme)):
-#     status = ()
-#     if status == True:
-#         return {'data': True, 'status': 'OK', 'code': 200}
-#     return {'data': False, 'status': 'OK', 'code': 200}
-
-### use folders
-
-
-# @app.get('/delete_folder/{folder}')
-# def delete_folder(folder: str, current_user: User = Depends(oauth2_scheme)):
-#     return {'data': folder, 'status': 'OK', 'code': 200}
-
-
-### use files
-
-
-# @app.get('/delete_file/{path}')
-# def upload_file(path: str, current_user: User = Depends(oauth2_scheme)):
-#     for file in files:
-#         if file.path == path:
-#             files.remove(file)
-#             return {'data': path, 'status': 'OK', 'code': 200}
-#     return get_bad_answer()
-
-
-
-# @app.post('/replace_file/{path}')
-# def replace_file(task: DataFile, path: str, current_user: User = Depends(oauth2_scheme)):
-#     data = task.data
-#     for file in files:
-#         if file.path == path:
-#             file.path = data
-#             return {'data': path, 'status': 'OK', 'code': 200}
-#     return get_bad_answer()
-
-
-### use grpc
-
-# test grpc
+# module gRPC #
 def gRPC_CreateUser(login: str, password: str, workspace_id: str):
     with grpc.insecure_channel('localhost:8785') as channel:
         stub = grpc_pb2.TransmissionStub(channel)
